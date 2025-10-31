8f3e889c6b79a613eb71d86b2131286884c66084
Tanisha Ali <136844678+tanisha971@users.noreply.github.com>
2025-10-31 23:45:29 +0530

    Enable CORS preflight requests for all routes

--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,7 @@ app.use(cors({
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
