6eefe9f2cac471f12b169e1b885590ad455e414b
Tanisha Ali <136844678+tanisha971@users.noreply.github.com>
2025-10-31 23:12:12 +0530

    Update CORS origin for backend

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    "https://civix-h1n4unprm-tanisha-alis-projects.vercel.app",
+    "https://civix-i3d9rt75e-tanisha-alis-projects.vercel.app/",
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
